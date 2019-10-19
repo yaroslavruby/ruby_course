a99f078c871a01b7c6066f017828fd7e2216178e
yaroslavruby <ohrimchuk77@gmail.com>
2019-10-19 14:28:12 +0200

    Change syntax

--- lesson_2/mage.rb
+++ lesson_2/mage.rb
@@ -1,9 +1,10 @@
 class Mage
- attr_accessor :name, :spell
- def enchant (target)
-  puts "#{@name} cats #{@spell} on #{target.name}!"
- end
-end 
+  attr_accessor :name, :spell
+
+  def enchant (target)
+    puts "#{@name} cats #{@spell} on #{target.name}!"
+  end
+end
 
 merlin = Mage.new
 merlin.name = "Merlin"
